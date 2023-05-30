c08da2ba801d6ef86a72461672a2ac6f20235818
vvvjh112 <lleell6@naver.com>
2023-05-30 17:25:23 +0900

    sort 생성

---
+++ sort.py
@@ -0,0 +1,13 @@
+
+def b2750():
+    a = int(input())
+    ary = []
+    for i in range(0,a):
+        ary.append(int(input()))
+    ary.sort()
+
+    for i in range(a):
+        print(ary[i])
+
+
+def b2587():
