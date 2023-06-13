31b8d3efa229730c58f2e8726cf1a103154a71d2
vvvjh112 <lleell6@naver.com>
2023-06-13 14:06:35 +0900

    save

--- Queue and Deque.py
+++ Queue and Deque.py
@@ -1,1 +1,71 @@
 import sys
+from collections import deque
+def b18258():
+    n = int(sys.stdin.readline())
+    result=[]
+    queue = deque([])
+    for i in range(n):
+        temp = list(sys.stdin.readline().split())
+        if len(temp)==2:
+            queue.append(temp[1])
+        elif temp[0]=="pop":
+            if len(queue) == 0:
+                result.append(-1)
+            else:
+                result.append(queue.popleft())
+        elif temp[0]=="size":
+            result.append(len(queue))
+        elif temp[0]=="empty":
+            if len(queue) == 0:
+                result.append(1)
+            else:
+                result.append(0)
+        elif temp[0]=="front":
+            if (len(queue) == 0):
+                result.append(-1)
+            else:
+                result.append(queue[0])
+        elif temp[0] == "back":
+            if (len(queue) == 0):
+                result.append(-1)
+            else:
+                result.append(queue[-1])
+
+    for i in range(len(result)):
+        print(result[i])
+
+
+def b2164():
+    a = int(sys.stdin.readline())
+    queue = deque([])
+    for i in range(1,a+1):
+        queue.append(i)
+
+    while(len(queue)!=1):
+        queue.popleft()
+        temp = queue.popleft()
+        queue.append(temp)
+
+    print(queue.pop())
+
+def b11866():
+    a,b = map(int,sys.stdin.readline().split())
+    queue = deque([])
+    result=[]
+    for i in range(1,a+1):
+        queue.append(i)
+
+    while (len(queue)!=0):
+        for i in range(b-1):
+            queue.append(queue.popleft())
+        result.append(queue.popleft())
+    print("<",end="")
+    for i in result:
+        if i == result[-1]:
+            print(result[-1],end="")
+            print(">")
+        else:
+            print(i, end=", ")
+
+
+b11866()
